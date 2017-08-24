6feab828aa73a9eb848ab8090a254ed63e096e8e
Plottel <matt.noone95@gmail.com>
2017-08-25 00:07:23 +1000

    NEW: GridCon -> Monitor multiple islands

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GridMovementMap = Dictionary<Grid, Vector2>;
+
+
+
+public class GridManager : MonoBehaviour
+{
+
+
+
+    public List<Grid> grids = new List<Grid>();
+
+    public GridMovementMap movingGrids = new GridMovementMap();
+
+
+
+    public void MoveGridTo(Grid grid, Vector2 pos)
+    {
+        if (grids.Contains(grid))
+        {
+
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
