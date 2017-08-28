5dfe58ce65d9846d5ebfc08d75d5dd46001c4109
Plottel <matt.noone95@gmail.com>
2017-08-28 13:44:53 +1000

    NEW: ListWrapper for serializing 2d lists

---
+++ Assets/Scripts/ListWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CellListWrapper
+{
+    [SerializeField]
+    public List<Cell> items = new List<Cell>();
+
+    public Cell this[int i]
+    {
+        get { return items[i]; }
+        set { items[i] = value; }
+    }
+}
