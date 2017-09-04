f60eabf0a6e9e88f025634c6e9638b3766b20174
Plottel <matt.noone95@gmail.com>
2017-09-04 11:34:41 +1000

    NEW: Visual countdown for time until next Grid Move

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour 
 {
@@ -10,6 +11,8 @@ public class LevelManager : MonoBehaviour
 
     public static System.Random RNG = new System.Random();
 
+    public Text timeToNextGridMove;
+
     // Use this for initialization
     void Start()
     {
@@ -17,12 +20,23 @@ public class LevelManager : MonoBehaviour
 
         _scene = new GridScene(grid);
 
-        TestAllMoves();
+
+        _scene.EnqueueMove(GridCon.SwapTwoCells, 5f, grid[RNG.Next(0, 8), RNG.Next(0, 8)], grid[RNG.Next(0, 8), RNG.Next(0, 8)]);
+        _scene.EnqueueMove(GridCon.SwapTwoCells, 7.65f, grid[RNG.Next(0, 8), RNG.Next(0, 8)], grid[RNG.Next(0, 8), RNG.Next(0, 8)]);
+        _scene.EnqueueMove(GridCon.SwapTwoCells, 3.9f, grid[RNG.Next(0, 8), RNG.Next(0, 8)], grid[RNG.Next(0, 8), RNG.Next(0, 8)]);
+        _scene.EnqueueMove(GridCon.SwapTwoCells, 1.23487f, grid[RNG.Next(0, 8), RNG.Next(0, 8)], grid[RNG.Next(0, 8), RNG.Next(0, 8)]);
+        _scene.EnqueueMove(GridCon.SwapTwoCells, 10.75f, grid[RNG.Next(0, 8), RNG.Next(0, 8)], grid[RNG.Next(0, 8), RNG.Next(0, 8)]);
+        _scene.EnqueueMove(GridCon.SwapTwoCells, 12f, grid[RNG.Next(0, 8), RNG.Next(0, 8)], grid[RNG.Next(0, 8), RNG.Next(0, 8)]);
+        _scene.EnqueueMove(GridCon.SwapTwoCells, 3.544f, grid[RNG.Next(0, 8), RNG.Next(0, 8)], grid[RNG.Next(0, 8), RNG.Next(0, 8)]);
+        _scene.EnqueueMove(GridCon.SwapTwoCells, 2.543258f, grid[RNG.Next(0, 8), RNG.Next(0, 8)], grid[RNG.Next(0, 8), RNG.Next(0, 8)]);
+
+        //TestAllMoves();
 
         // There's an extra shake somewhere.
         // Definitely affects SplitGridIntoFour()
         // Possibly affects ReformGrid()
         // SwapTwoCells() is fine
+        UpdateTimeToNextGridMove();
 
     }
 
@@ -45,5 +59,11 @@ public class LevelManager : MonoBehaviour
             _scene.Start();
 
         _scene.Play();
+        UpdateTimeToNextGridMove();
+    }
+
+    void UpdateTimeToNextGridMove()
+    {
+        timeToNextGridMove.text = "Grid Move In: " + _scene.TimeToNextMove.ToString();  // + Get some value from GridScene here
     }
 }
