86a884fb25dd8ec78e2884445576f6d4de5b8236
Aaron Caratelli <aaron.caratelli@gmail.com>
2017-09-07 19:32:25 +1000

    New Player movement
    Began moving all controls over to the force based movement system.
    Still requires attack functionality and tweaking.

--- Assets/Scripts/MovementControl.cs
+++ Assets/Scripts/MovementControl.cs
@@ -6,10 +6,11 @@ using UnityStandardAssets.Characters.ThirdPerson;
 
 public class MovementControl : MonoBehaviour {
 
-	private ThirdPersonCharacter m_Character; 			// A reference to the Player Abilities on the game object
-	private Transform m_Cam;                // A reference to the main camera in the scene
-	private Vector3 m_CamForward;           // The current forward direction of the camera
+	private Player m_Character; 		// A reference to the Player Abilities on the game object
+	private Transform m_Cam;                		// A reference to the main camera in the scene
+	private Vector3 m_CamForward;           		// The current forward direction of the camera
 	private Vector3 m_Move;
+
 
 	private bool m_Jump;                    // the world-relative desired move direction, calculated from the camForward and user input.
 	private bool m_attack_area;
@@ -33,13 +34,13 @@ public class MovementControl : MonoBehaviour {
 		}
 
 		// get the third person character ( this should never be null due to require component )
-		m_Character = GetComponent<ThirdPersonCharacter>();
+		m_Character = GetComponent<Player>();
 	}
 
 
 	private void Update()
 	{
-		if (!m_Jump)
+		if (!m_Jump) 
 		{
 			m_Jump = CrossPlatformInputManager.GetButtonDown(Player + "_Jump");
 		}
@@ -76,14 +77,14 @@ public class MovementControl : MonoBehaviour {
 		// walk speed multiplier
 		if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
 		#endif
-
 		// pass all parameters to the character control script
-		m_Character.Move(m_Move, crouch, m_Jump);
+		m_Character.Move(m_Move, m_Jump);
 		m_Jump = false;
 
 		if (a_direct_attack) 
 		{
-			var attack = GameObject.Instantiate(AttackCone, this.gameObject.transform.position, this.gameObject.transform.rotation, this.gameObject.transform);
+			var attack = GameObject.Instantiate(AttackCone, this.gameObject.transform.position, this.gameObject.transform.localRotation, this.gameObject.transform);
+			a_direct_attack = false;
 		}
 	}
 }
