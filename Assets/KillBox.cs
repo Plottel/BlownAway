565b7b861133411674e229d3add584c88c1b6d72
Jake Scott <coolasjake8@gmail.com>
2017-09-09 21:43:48 +1000

    Quick fixes to have multiplayer, respawning and the new movement working together

---
+++ Assets/KillBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour {
+
+	public MultiplayerController MultiplayerController;
+
+	protected void OnCollisionEnter (Collision col) {
+		MovementControl P = col.gameObject.GetComponent<MovementControl> ();
+		//Debug.Log ("Collision with: " + P.name);
+		if (P) {
+			MultiplayerController.KillPlayerByString (P.Player);
+			Destroy (col.gameObject);
+			//Debug.Log ("Killed Player");
+		}
+	}
+}
--- Assets/Scripts/MultiplayerController.cs
+++ Assets/Scripts/MultiplayerController.cs
@@ -7,60 +7,135 @@ using UnityEngine.UI;
 public class MultiplayerController : MonoBehaviour {
 
 	public GameObject PlayerPrefab;
-	private bool Player1 = false;
-	private bool Player2 = false;
-	private bool Player3 = false;
-	private bool Player4 = false;
-	/*
-	public GameObject Player1Join;
-	public GameObject Player2Join;
-	public GameObject Player3Join;
-	public GameObject Player4Join;
-	*/
+	public GameObject StockPrefab;
+	private bool[] ActivePlayers = new bool[4];
+	public int StartingLives = 4;
+	private int MaxLives = 4;
+	private int[] Lives = new int[4];
+
+	private Image[,] Stocks = new Image[4,4];
 
 	// Use this for initialization
 	void Start () {
-		
+		if (StartingLives > MaxLives) {
+			StartingLives = MaxLives;
+		}
+
+		for (int i = 0; i <= 3; i++) {
+			Lives [i] = 0;
+		}
+
+		int k = 4;
+		for (int i = 0; i <= 3; i++) {
+			int j = 0;
+			Stocks [i,j] = Instantiate (StockPrefab).GetComponent<Image>();
+			Stocks [i,j].enabled = false;
+			j += 1;
+			k += 1;
+			Stocks[i,j] = Instantiate (StockPrefab).GetComponent<Image>();
+			Stocks [i,j].enabled = false;
+			j += 1;
+			k += 1;
+			Stocks[i,j] = Instantiate (StockPrefab).GetComponent<Image>();
+			Stocks [i,j].enabled = false;
+			j += 1;
+			k += 1;
+			Stocks[i,j] = Instantiate (StockPrefab).GetComponent<Image>();
+			Stocks [i,j].enabled = false;
+			j += 1;
+			k += 1;
+		}
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
-		if (!Player1) {
-			if (Player1 = CrossPlatformInputManager.GetButtonDown ("P1_Start")) {
-				GameObject P = Instantiate (PlayerPrefab);
-				P.GetComponent<MovementControl> ().Player = "P1";
-				P.GetComponent<Transform> ().position = new Vector3 (0, 5, -10);
+		for (int p = 0; p <= 3; p++) {
+			if (!ActivePlayers [p]) {
+				if (ActivePlayers [p] = CrossPlatformInputManager.GetButtonDown ("P" + (p+1) + "_Start")) {
 
-				GetComponentsInChildren<Image> () [0].enabled = false;
-			}
-		}
-		if (!Player2) {
-			if (Player2 = CrossPlatformInputManager.GetButtonDown ("P2_Start")) {
-				GameObject P = Instantiate (PlayerPrefab);
-				P.GetComponent<MovementControl> ().Player = "P2";
-				P.GetComponent<Transform> ().position = new Vector3 (10, 5, -10);
+					CreatePlayer (p);
+					Debug.Log ("Created in Update");
 
-				GetComponentsInChildren<Image> () [1].enabled = false;
-			}
-		}
-		if (!Player3) {
-			if (Player3 = CrossPlatformInputManager.GetButtonDown ("P3_Start")) {
-				GameObject P = Instantiate (PlayerPrefab);
-				P.GetComponent<MovementControl> ().Player = "P3";
-				P.GetComponent<Transform> ().position = new Vector3 (10, 5, 0);
+					GetComponentsInChildren<Image> () [p].enabled = false;
 
-				GetComponentsInChildren<Image> () [2].enabled = false;
-			}
-		}
-		if (!Player4) {
-			if (Player4 = CrossPlatformInputManager.GetButtonDown ("P4_Start")) {
-				GameObject P = Instantiate (PlayerPrefab);
-				P.GetComponent<MovementControl> ().Player = "P4";
-				P.GetComponent<Transform> ().position = new Vector3 (0, 5, -0);
+					Lives [p] = StartingLives;
+					ActivePlayers [p] = true;
 
-				GetComponentsInChildren<Image> () [3].enabled = false;
+					UpdateStockGraphics ();
+					/*
+					for (int i = 0; i <= 3; i++) {
+						Stocks [p, i].enabled = true;
+					}
+					*/
+
+				}
 			}
 		}
 	}
+
+	//Show or hide stocks based on the number of lives the player has.
+	private void UpdateStockGraphics() {
+		for (int p = 0; p <= 3; p++) {
+			for (int i = 0; i <= 3; i++) {
+				Stocks [p, i].enabled = false;
+			}
+			for (int i = 0; i < Lives[p]; i++) {
+				Stocks [p, i].enabled = true;
+			}
+		}
+	}
+
+	//CHANGE THE SPAWN POSITION OF PLAYERS HERE (for now).
+	private void CreatePlayer(int Player) {
+		Debug.Log ("Created: " + Player);
+		if (Player == 0) {
+			GameObject P = Instantiate (PlayerPrefab);
+			P.GetComponent<MovementControl> ().Player = "P1";
+			P.GetComponent<Transform> ().position = new Vector3 (0, 5, -10);
+		} else if (Player == 1) {
+			GameObject P = Instantiate (PlayerPrefab);
+			P.GetComponent<MovementControl> ().Player = "P2";
+			P.GetComponent<Transform> ().position = new Vector3 (10, 5, -10);
+		} else if (Player == 2) {
+			GameObject P = Instantiate (PlayerPrefab);
+			P.GetComponent<MovementControl> ().Player = "P3";
+			P.GetComponent<Transform> ().position = new Vector3 (10, 5, 0);
+		} else if (Player == 3) {
+			GameObject P = Instantiate (PlayerPrefab);
+			P.GetComponent<MovementControl> ().Player = "P4";
+			P.GetComponent<Transform> ().position = new Vector3 (0, 5, -0);
+		}
+	}
+
+	public void KillPlayerByString (string PName) {
+		int PNumber = 0;
+		if (PName == "P1") {
+			PNumber = 0;
+		} else if (PName == "P2") {
+			PNumber = 1;
+		} else if (PName == "P3") {
+			PNumber = 2;
+		} else if (PName == "P4") {
+			PNumber = 3;
+		}
+		Lives [PNumber] -= 1;
+		if (Lives [PNumber] != 0) {
+			CreatePlayer (PNumber);
+			Debug.Log ("Created in String");
+		}
+		UpdateStockGraphics ();
+		Debug.Log ("Name: " + PName + ", Number: " + PNumber + ", Lives: " + Lives [PNumber]);
+	}
+
+	public void KillPlayerByInt (int PNumber) {
+		Lives [PNumber] -= 1;
+		if (Lives [PNumber] != 0) {
+			CreatePlayer (PNumber);
+			Debug.Log ("Created in Int");
+		}
+		UpdateStockGraphics ();
+	}
+
 }
