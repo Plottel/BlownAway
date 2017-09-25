639651a5502dc2143657256d4caac7ec76ed0131
Plottel <matt.noone95@gmail.com>
2017-09-25 20:42:57 +1000

    NEW: Tutorial-grid-scene complete - Text prompts commented

--- Assets/Scripts/GridFactory.cs
+++ Assets/Scripts/GridFactory.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public static class GridFactory
 {
+    public static Cell MakeTerrainCellAt(Vector3 pos, TerrainType type)
+    {
+        if (type == TerrainType.Ballista)
+            return MakeBallistaCellAt(pos);
+        else if (type == TerrainType.Tree)
+            return MakeTreeCellAt(pos);
+        else
+            return MakeSpikyBushCellAt(pos);
+    }
+
     public static Cell MakeBallistaCellAt(Vector3 pos)
     {
         Cell c = Object.Instantiate(Prefabs.Cell, pos, Quaternion.identity);
@@ -22,5 +32,28 @@ public static class GridFactory
         c.islandPiece.AddTerrain(TerrainType.Tree);
 
         return c;
+    }
+
+    public static Cell MakeSpikyBushCellAt(Vector3 pos)
+    {
+        Cell c = Object.Instantiate(Prefabs.Cell, pos, Quaternion.identity);
+
+        c.AddIslandPiece();
+        c.islandPiece.AddTerrain(TerrainType.SpikyBush);
+
+        return c;
+    }
+
+    public static Cell MakeIslandPieceCellAt(Vector3 pos)
+    {
+        Cell c = Object.Instantiate(Prefabs.Cell, pos, Quaternion.identity);
+        c.AddIslandPiece();
+
+        return c;
+    }
+
+    public static Cell MakeEmptyCellAt(Vector3 pos)
+    {
+        return Object.Instantiate(Prefabs.Cell, pos, Quaternion.identity);
     }
 }
