46a44f5a1948711a2f29bb2696eb0e6c8537bb85
Jake Scott <coolasjake8@gmail.com>
2017-09-26 16:21:10 +1000

    SpawnPoint Started

---
+++ Assets/Scripts/SpawnPointer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class SpawnPointer : MonoBehaviour {
+
+	public bool OverTile = false;
+	public string Player = "P3";
+	
+	// Update is called once per frame
+
+	//public bool Manual
+	void Update () {
+
+		float h = CrossPlatformInputManager.GetAxis(Player + "_Horizontal");
+		float v = CrossPlatformInputManager.GetAxis(Player + "_Vertical");
+
+		transform.position = new Vector3 (transform.position.x + h, transform.position.y, transform.position.z + v);
+
+		RaycastHit Hit;
+
+		Vector3 RayStart = new Vector3 (transform.position.x, 5, transform.position.z);
+
+		if (Physics.Raycast (RayStart, Vector3.down * 6, out Hit, 6)) {
+			Debug.DrawRay (RayStart, Vector3.down * 6, Color.red);
+			transform.position = Hit.point;
+			//return true;
+		} else {
+			//return false;
+		}
+	}
+}
