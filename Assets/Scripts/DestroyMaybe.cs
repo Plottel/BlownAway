7cc3bb3a303d89db839703cc88f43cefe4922fee
Aaron Caratelli <aaron.caratelli@gmail.com>
2017-10-05 15:17:27 +1100

    Grass Maybe
    Patchy grass on levels

---
+++ Assets/Scripts/DestroyMaybe.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMaybe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int chance = Random.Range (0, 2);
+		if (chance == 0)
+			Destroy (this.gameObject);
+	}
+}
