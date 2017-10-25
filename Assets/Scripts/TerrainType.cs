9c5dd4717ce19f93863227a044424e36f537a4c0
Aaron Caratelli <100939078@student.swin.edu.au>
2017-10-25 12:43:39 +1100

    Final 3D assets

---
+++ Assets/Scripts/TerrainType.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TerrainType
+{
+	SpikyBush,
+	Tree,
+	Ballista,
+	Piston,
+	Fan,
+	PressurePlate,
+	Lava,
+	LavaPipe,
+	Volcano,
+	Wall,
+	None
+}
