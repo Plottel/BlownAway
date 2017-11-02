f8f61c5389a876aa195bbbf3f2852716208cb4bf
Aaron Caratelli <100939078@student.swin.edu.au>
2017-11-02 12:54:35 +1100

    Music Player Upgrade
    Added functionality for muslitple music types

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class MusicPlayer : MonoBehaviour {
 
 	static MusicPlayer instance = null;
+	public string Music;
 
 	void Awake()
 	{
-		Debug.Log ("Music player Awake " + GetInstanceID ());
-		if (instance != null) {
-			Destroy (gameObject);
-			print ("Duplicate MusicPlayer destroyed.");
-		} else {
+		//Debug.Log ("Music player Awake " + GetInstanceID ());
+		if (instance != null) 
+		{
+			if (instance.Music == this.Music) 
+			{
+				Destroy (gameObject);
+				//Debug.Log ("Duplicate MusicPlayer destroyed.");
+			} 
+			else 
+			{
+				Destroy (instance.gameObject);
+				instance = this;
+				GameObject.DontDestroyOnLoad (gameObject);
+			}
+		} 
+		else 
+		{
 			instance = this;
 			GameObject.DontDestroyOnLoad (gameObject);
 		}
@@ -21,7 +34,7 @@ public class MusicPlayer : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		Debug.Log ("Music Player Start " + GetInstanceID ());
+		//Debug.Log ("Music Player Start " + GetInstanceID ());
 	}
 
 	// Update is called once per frame
