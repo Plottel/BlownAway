b449146592cc0649fb609dfa17be184e61257b85
Jake Scott <coolasjake8@gmail.com>
2017-11-16 22:02:21 +1100

    Arrows that show which direction you're off the screen

--- Assets/Scripts/PlayerIcon.cs
+++ Assets/Scripts/PlayerIcon.cs
@@ -13,6 +13,9 @@ public class PlayerIcon : MonoBehaviour {
 	public string Name;
 	public int PlayerNumber;
 
+	public GameObject ChevronPrefab;
+	private GameObject ChevronInstance;
+
 	private Text TextObject;
 	private float Health = 0;
 
@@ -20,12 +23,62 @@ public class PlayerIcon : MonoBehaviour {
 	void Start() {
 		TextObject = GetComponent<Text> ();
 		TextObject.color = Player.ChooseColor (PlayerNumber - 1);
+		ChevronInstance = Instantiate (ChevronPrefab, transform);
+		ChevronInstance.GetComponent<Image>().color = Player.ChooseColor (PlayerNumber - 1);
+		ChevronInstance.GetComponent<Image> ().enabled = false;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		Vector3 wantedPos = Camera.main.WorldToScreenPoint (Target.position);
 		transform.position = wantedPos;
+
+
+		bool OffScreen = false;
+		float DesiredAngle = -1;
+
+		if (transform.position.x > Screen.width) {
+			Debug.Log ("OutsideScreen Right");
+			//transform.position = new Vector3 (Screen.width - 100, transform.position.y, transform.position.z);
+			ChevronInstance.transform.position = new Vector3 (Screen.width - 100, transform.position.y, transform.position.z);
+			DesiredAngle = 270;
+			OffScreen = true;
+		} else if (transform.position.x < 0) {
+			Debug.Log ("OutsideScreen Left");
+			ChevronInstance.transform.position = new Vector3 (0 + 100, transform.position.y, transform.position.z);
+			DesiredAngle = 90;
+			OffScreen = true;
+		}
+
+		if (transform.position.y > Screen.height) {
+			Debug.Log ("OutsideScreen Top ?");
+			ChevronInstance.transform.position = new Vector3 (transform.position.x, Screen.width - 70, transform.position.z);
+			if (DesiredAngle == -1)
+				DesiredAngle = 0;
+			else
+				DesiredAngle = DesiredAngle / 2;
+			OffScreen = true;
+		} else if (transform.position.y < 0) {
+			Debug.Log ("OutsideScreen Bottom");
+			ChevronInstance.transform.position = new Vector3 (transform.position.x, 0 + 70, transform.position.z);
+			if (DesiredAngle == -1)
+				DesiredAngle = 180;
+			else
+				DesiredAngle = (DesiredAngle + 180) / 2;
+			OffScreen = true;
+		}
+
+		if (OffScreen) {
+			ChevronInstance.GetComponent<Image> ().enabled = true;
+			ChevronInstance.transform.rotation = new Quaternion ();
+			ChevronInstance.transform.Rotate (0, 0, DesiredAngle);
+		} else {
+			ChevronInstance.GetComponent<Image> ().enabled = false;
+		}
+
+		//height is 100
+		//halve and add 20 is *70*
+
 		SetHealth = Target.GetComponent<Player> ().Health;
 
 		if (ChangeText) {
