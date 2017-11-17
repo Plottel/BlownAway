c27b07f7641ed05d47ea5bf849e3106e2c9bf96d
Jake Scott <coolasjake8@gmail.com>
2017-11-18 00:34:44 +1100

    Coloured hearts that break on death.

---
+++ Assets/Scripts/FallAndFade.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FallAndFade : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3 (0, -2, 0);
+		Image I = GetComponent<Image> ();
+		I.color = new Color(I.color.r, I.color.g, I.color.b, I.color.a - 0.02f);
+		if (I.color.a <= 0)
+			Destroy (gameObject);
+	}
+}
